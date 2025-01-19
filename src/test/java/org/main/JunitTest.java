ad77f3dec16b8313f4f8b8a8a4d9a1969f43a3db
Suki0203 <lokeshwaranm98@gmail.com>
2025-01-19 15:49:04 +0530

    junit commit

---
+++ src/test/java/org/main/JunitTest.java
@@ -0,0 +1,11 @@
+package org.main;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ClassATestNg.class,ClassBTestNg.class,ClassCTestNg.class})
+public class JunitTest {
+
+}
